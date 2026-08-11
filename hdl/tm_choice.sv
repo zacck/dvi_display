9661d11560186487fcc62baa49c410f6c2552ddb
Zacck Osiemo <coderv63@gmail.com>
2026-08-11 22:07:40 +0200

    hdl: sim: Add sim and hdl for tmds tm part
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

---
+++ hdl/tm_choice.sv
@@ -0,0 +1,35 @@
+`default_nettype none
+module tm_choice (
+	input wire [7:0] din,
+	output logic [8:0] q_m
+);
+	wire [3:0] count; 
+
+	//assign count = din[0] + din[1] + din[2] + din[3] + din[4] + din[5] + din[6] + din[7]; 
+	assign count = $countones(din);
+	always_comb begin
+		if((count == 4) || ((count > 4)  && (din[0] == 0))) begin
+			q_m[0] = din[0];
+			q_m[1] = ~(din[1] ^ q_m[0]);
+			q_m[2] = ~(din[2] ^ q_m[1]);
+			q_m[3] = ~(din[3] ^ q_m[2]);
+			q_m[4] = ~(din[4] ^ q_m[3]);
+			q_m[5] = ~(din[5] ^ q_m[4]);
+			q_m[6] = ~(din[7] ^ q_m[5]);
+			q_m[7] = ~(din[7] ^ q_m[6]);
+			q_m[8] = 0;
+
+		end else begin
+			q_m[0] = din[0];
+			q_m[1] = (din[1] ^ q_m[0]);
+			q_m[2] = (din[2] ^ q_m[1]);
+			q_m[3] = (din[3] ^ q_m[2]);
+			q_m[4] = (din[4] ^ q_m[3]);
+			q_m[5] = (din[5] ^ q_m[4]);
+			q_m[6] = (din[7] ^ q_m[5]);
+			q_m[7] = (din[7] ^ q_m[6]);
+			q_m[8] = 1;
+		end
+	end 	
+endmodule
+`default_nettype wire
---
+++ sim/test_tm_choice.py
@@ -0,0 +1,59 @@
+import cocotb
+import os
+import random 
+import sys 
+from math import log 
+import logging
+from pathlib import Path 
+from cocotb.clock import Clock 
+from cocotb.triggers import Timer, ClockCycles, RisingEdge, FallingEdge
+from cocotb.runner import get_runner
+test_file = os.path.basename(__file__).replace(".py", "")
+
+@cocotb.test()
+async def test_tm_choice_one(dut):
+    """Is out tm choice correct"""
+    dut._log.info("Starting...")
+    await Timer(5, "ns")
+    dut._log.info("Starting Option 1")
+    dut.din.value = 0x01
+    await Timer(5, "ns")
+    assert dut.q_m.value == 0x1FF
+    await Timer(5, "ns")
+    dut._log.info("Starting Option 2")
+    dut.din.value = 0xFE
+    await Timer(5, "ns")
+    assert dut.q_m.value == 0x00
+   
+def tm_choice_runner(): 
+    """ Simulate the tm_choice """
+    hdl_toplevel_lang = os.getenv("HDL_TOPLEVEL_LANG", "verilog")
+    sim = os.getenv("SIM", "verilator")
+    proj_path = Path(__file__).resolve().parent.parent
+    sys.path.append(str(proj_path / "sim" / "model"))
+    sources = [proj_path / "hdl" / "tm_choice.sv"]
+    hdl_toplevel = "tm_choice"
+    build_test_args = ["-Wall"]
+    parameters = {}
+    sys.path.append(str(proj_path  / "sim"))
+    runner = get_runner(sim)
+    runner.build(
+            sources=sources,
+            hdl_toplevel=hdl_toplevel,
+            always=True, 
+            build_args=build_test_args, 
+            parameters=parameters, 
+            timescale=('1ns', '1ps'),
+            waves=True
+    )
+    run_test_args=[]
+    runner.test(
+            hdl_toplevel=hdl_toplevel, 
+            test_module=test_file,
+            test_args=run_test_args, 
+            waves=True
+    )
+
+
+if __name__ == "__main__":
+    tm_choice_runner()
