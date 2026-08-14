1c8c55b63ef89fef3358757eff5243aa9687ecd6
Zacck Osiemo <coderv63@gmail.com>
2026-08-14 22:52:48 +0200

    hdl: Instantiate SERDES and MMCME for serialization and clocking
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

---
+++ hdl/hdmi_clk_wiz.v
@@ -0,0 +1,132 @@
+`default_nettype none
+module hdmi_clk_wiz_720p(
+	output wire clk_pixel, 
+	output wire clk_tmds,
+	//CSR 
+	input wire reset, 
+	output wire locked,
+	input wire clk_ref
+);
+	wire clk_ref_clk_wiz_0;
+	wire clk_in2_clk_wiz_0;
+
+	IBUF clkin1_ibufg(
+		.O(clk_ref_clk_wiz_0),
+		.I(clk_ref)
+	);
+
+	// MMCM 
+	wire clk_pixel_clk_wiz_0;
+	wire clk_tmds_clk_wiz_0;
+	wire clk_out3_clk_wiz_0;
+	wire clk_out4_clk_wiz_0;
+	wire clk_out5_clk_wiz_0;
+	wire clk_out6_clk_wiz_0;
+	wire clk_out7_clk_wiz_0;
+
+	wire [15:0] do_unused;
+	wire drdy_unused;
+	wire psdone_unused;
+	wire locked_int;
+	wire clkfbout_clk_wiz_0;
+	wire clkfbout_buf_clk_wiz_0;
+	wire clkfboutb_unused;
+	wire clkout0b_unused;
+	wire clkout1b_unused;
+	wire clkout2_unused;
+	wire clkout2b_unused;
+	wire clkout3_unused;
+	wire clkout3b_unused;
+	wire clkout4_unused;
+	wire clkout5_unused;
+	wire clkout6_unused;
+	wire clkfbstopped_unused;
+	wire clkinstopped_unused;
+	wire reset_high; 
+
+	MMCME2_ADV
+	#(
+		.BANDWIDTH("OPTIMIZED"),
+		.CLKOUT4_CASCADE("FALSE"),
+		.COMPENSATION("ZHOLD"),
+		.STARTUP_WAIT("FALSE"),
+		.DIVCLK_DIVIDE(5),
+		.CLKFBOUT_MULT_F(37.125),
+		.CLKFBOUT_PHASE(0.000),
+		.CLKFBOUT_USE_FINE_PS("FALSE"),
+		.CLKOUT0_DIVIDE_F(10.000),
+		.CLKOUT0_PHASE(0.000),
+		.CLKOUT0_DUTY_CYCLE(0.500),
+		.CLKOUT1_USE_FINE_PS("FALSE"),
+		.CLKIN1_PERIOD(10.000)
+	) mmcm_adv_inst (
+		//Output Clocks
+		.CLKFBOUT(clkfbout_clk_wiz_0),
+		.CLKFBOUTB(clkfboutb_unused),
+		.CLKOUT0(clk_pixel_clk_wiz_0),
+		.CLKOUT0B(clkout0b_unused),
+		.CLKOUT1(clk_tmds_clk_wiz_0),
+		.CLKOUT1B(clkout1b_unused),
+		.CLKOUT2(clkout2_unused),
+		.CLKOUT2B(clkout2b_unused),
+		.CLKOUT3(clkout3_unused),
+		.CLKOUT3B(clkout3b_unused),
+		.CLKOUT4(clkout4_unused),
+		.CLKOUT5(clkout5_unused),
+		.CLKOUT6(clkout6_unused),
+		//Input clock control
+		.CLKFBIN(clkfbout_buf_clk_wiz_0),
+		.CLKIN1(clk_ref_clk_wiz_0),
+		.CLKIN2(1'b0),
+		// Tied to select the primary clock 
+		.CLKINSEL(1'b1),
+		//Reconfiguration ports 
+		.DADDR(7'h0),
+		.DCLK(1'b0),
+		.DEN(1'b0),
+		.DI(16'h0),
+		.DO(do_unused),
+		.DRDY(drdy_unused),
+		.DWE(1'b0),
+		// Phase shift ports 
+		.PSCLK(1'b0),
+		.PSEN(1'b0),
+		.PSINCDEC(1'b0),
+		.PSDONE(psdone_unused),
+		//CSR Signals
+		.LOCKED(locked_int),
+		.CLKINSTOPPED(clkinstopped_unused),
+		.CLKFBSTOPPED(clkfbstopped_unused),
+		.PWRDWN(1'b0),
+		.RST(reset_high)
+	);
+
+	assign reset_high = reset;
+	assign locked = locked_int;
+
+	//OUTPUT Buffering 
+	BUFG clkf_buf(
+		.O(clkfbout_buf_clk_wiz_0),
+		.I(clkfbout_clk_wiz_0)
+	);
+
+	BUFG clkout1_buf(
+		.O(clk_pixel),
+		.I(clk_pixel_clk_wiz_0)
+	);
+
+	BUFG clkout2_buf(
+		.O(clk_tmds),
+		.I(clk_tmds_clk_wiz_0)
+	);
+	
+
+
+
+
+
+
+
+
+endmodule
+`default_nettype wire
---
+++ hdl/tmds_serializer.sv
@@ -0,0 +1,95 @@
+`default_nettype none
+module tmds_serializer(
+	input wire clk_pixel, 
+	input wire clk_5x, 
+	input wire rst, 
+	input wire [9:0] tmds_in, 
+	output logic tmds_out
+	);
+
+	logic[1:0] linker;
+
+	//required for OSERDERS2
+	logic pwup_rst = 1'b1;
+	always @(posedge clk_pixel) begin 
+		pwup_rst <= 1'b0;
+	end
+
+	OSERDESE2 #(
+		.DATA_RATE_OQ("DDR"),
+		.DATA_RATE_TQ("SDR"),
+		.DATA_WIDTH(10),
+		.SERDES_MODE("MASTER"),
+		.TRISTATE_WIDTH(1),
+		.TBYTE_CTL("FALSE"), 
+		.TBYTE_SRC("FALSE")
+	) primary (
+		.OQ(tmds_out),
+		.OFB(),
+		.TQ(),
+		.TFB(),
+		.SHIFTOUT1(),
+		.SHIFTOUT2(),
+		.TBYTEOUT(),
+		.CLK(clk_5x),
+		.CLKDIV(clk_pixel), 
+		.D1(tmds_in[0]), 
+		.D2(tmds_in[1]), 
+		.D3(tmds_in[2]), 
+		.D4(tmds_in[3]),
+		.D5(tmds_in[4]),
+		.D6(tmds_in[5]),
+		.D7(tmds_in[6]),
+		.D8(tmds_in[7]),
+		.TCE(1'b0),
+		.OCE(1'b0),
+		.TBYTEIN(1'b0),
+		.RST(rst || pwup_rst),
+		.SHIFTIN1(linker[0]),
+		.SHIFTIN2(linker[1]),
+		.T1(1'b0),
+		.T2(1'b0),
+		.T3(1'b0),
+		.T4(1'b0)
+	);
+
+
+	OSERDESE2 #(
+		.DATA_RATE_OQ("DDR"),
+		.DATA_RATE_TQ("SDR"),
+		.DATA_WIDTH(10),
+		.SERDES_MODE("SLAVE"),
+		.TRISTATE_WIDTH(1),
+		.TBYTE_CTL("FALSE"),
+		.TBYTE_SRC("FALSE")
+	) secondary (
+		.OQ(),
+		.OFB(),
+		.TQ(),
+		.TFB(),
+		.SHIFTOUT1(linker[0]),
+		.SHIFTOUT2(linker[1]),
+		.TBYTEOUT(),
+		.CLK(clk_5x),
+		.CLKDIV(clk_pixel),
+		.D1(1'b0),
+		.D2(1'b0),
+		.D3(tmds_in[8]),
+		.D4(tmds_in[9]),
+		.D5(1'b0),
+		.D6(1'b0),
+		.D7(1'b0),
+		.D8(1'b0),
+		.TCE(1'b0),
+		.OCE(1'b1),
+		.TBYTEIN(1'b0),
+		.RST(rst || pwup_rst),
+		.SHIFTIN1(1'b0),
+		.SHIFTIN2(1'b0),
+		.T1(1'b0),
+		.T2(1'b0),
+		.T3(1'b0),
+		.T4(1'b0)
+	);
+endmodule
+`default_nettype wire
